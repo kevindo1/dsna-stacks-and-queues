2ff9ba27a080419fe595ced7f1c99c250972bb2a
Kevin Do <kevind0501@gmail.com>
2022-04-05 15:48:31 -0700

    binary tree node

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
-class Queue {
-  constructor() {
-    this.queue = [];
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//   }
+
+//   enqueue(item) {
+//     return this.queue.push(item);
+//   }
+
+//   dequeue() {
+//     return this.queue.length ? this.queue.shift() : null;
+//   }
+
+//   peek() {
+//     return this.queue[this.length - 1];
+//   }
+
+//   get length() {
+//     return this.queue.length;
+//   }
+
+//   isEmpty() {
+//     return this.queue.length === 0;
+//   }
+// }
+
+class BinaryTreeNode {
+  constructor(value) {
+    // adding value, left, right properties through the constructor
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 
-  enqueue(item) {
-    return this.queue.push(item);
-  }
-
-  dequeue() {
-    return this.queue.length ? this.queue.shift() : null;
-  }
-
-  peek() {
-    return this.queue[this.length - 1];
-  }
-
-  get length() {
-    return this.queue.length;
-  }
-
-  isEmpty() {
-    return this.queue.length === 0;
+  add(node) {
+    // Is there a next property?
+    if (!this.left) {
+      // if no, then this right node can be our next
+      this.right = node;
+    } else {
+      // if yes, then go to current next node
+      this.left.add(node);
+    }
   }
 }
+
+// Test Cases
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+// B will be the root of the tree:
+B.add(A);
+B.add(D);
+B.add(C);
