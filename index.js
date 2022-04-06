e5b31eeee7bf19f6f2209dc3717cb2ea7b5097a1
Kevin Do <kevind0501@gmail.com>
2022-04-05 18:14:07 -0700

    attempted person tree node

--- index.js
+++ index.js
@@ -42,3 +42,32 @@ class LinkedListNode {
     }
   }
 }
+
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+  // add node
+  add(node) {
+    // assume value already in tree, nothing to add
+    if(node.value === this.value) {
+      return;
+    }
+
+    // adding to left and to right
+    if(node.value < this.value) {
+      if(!this.left) this.left = node;
+      else this.left.add(node);
+    } else {
+      if(!this.right) this.right = node;
+      else this.right.add(node);
+    }
+  }
+  // find person node
+  findPerson(name) {
+    return this.value === name ? this.person
+  }
+}
